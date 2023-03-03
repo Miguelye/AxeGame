eb1a1fed2c6a4f500196363d7b79abf9a9f51c7b
Miguel Yepes <miguelyegarcia94@gmail.com>
2023-03-03 03:58:11 -0700

    Added 2D movement to circle and created an axe(Rectangle)

--- game/src/raylib_game.cpp
+++ game/src/raylib_game.cpp
@@ -1,22 +1,75 @@
 #include "raylib.h"
 #include <string>
+#include <iostream>
 
 using namespace std;
 
-int width = 350;
-int length = 200;
 
 int main()
 {
-	InitWindow(width, length, "Miguel's Window");
 
-	
+	//Window Dimensions
+	int windowWidth = 800;
+	int windowHeight = 450;
+	InitWindow(windowWidth, windowHeight, "Miguel's Window");
 
+	//Circle Data
+	int circleX = windowWidth / 2;
+	int circleY = windowHeight / 2;
+	int radius = 20;
+
+	//Rectangle Data
+	int rectX = 300;
+	int rectY = 0;
+	int rectHeight = 50;
+	int rectWidth = 50;
+
+	SetTargetFPS(60);
 	while (!WindowShouldClose())
 	{
+		
 		BeginDrawing();
 		ClearBackground(WHITE);
-		DrawCircle(width/2, length/2, 20, BLUE);
+
+		//Game Logic Begins
+		DrawCircle(circleX, circleY, radius, BLUE);
+		DrawRectangle(rectX, rectY, rectWidth, rectHeight, RED);
+
+		
+		
+		rectY += 5;
+
+		if (IsKeyDown(KEY_D))
+		{
+			if (circleX < windowWidth)
+				circleX += 5;
+			else
+				circleX = 0;
+		}		
+		if (IsKeyDown(KEY_A))
+		{
+			if (circleX > 0)
+				circleX -= 5;
+			else
+				circleX = windowWidth;
+		}		
+		if (IsKeyDown(KEY_W))
+		{
+			if (circleY > 0)
+				circleY -= 5;
+			else
+				circleY = windowHeight;
+		}		
+		if (IsKeyDown(KEY_S))
+		{
+			if (circleY < windowHeight)
+				circleY += 5;
+			else
+				circleY = 0;
+		}
+
+
+
 		EndDrawing();
 	}
 	return 0;
